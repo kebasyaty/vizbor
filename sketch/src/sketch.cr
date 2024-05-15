270044d036bc80fc4cb32d5a33215fbc587397d6
kebasyaty <kebasyaty@gmail.com>
2024-05-15 15:32:12 +0300

    update

--- sketch/src/sketch.cr
+++ sketch/src/sketch.cr
@@ -1,5 +1,6 @@
 require "kemal"
 require "dynfork"
+require "./sketch/settings"
 
 module Sketch
   VERSION = "0.1.0"
--- sketch/src/sketch/settings.cr
+++ sketch/src/sketch/settings.cr
@@ -1,4 +1,7 @@
 # Settings for your web application.
 module Sketch::Settings
-  # ...
+  class_getter app_name : String = ""
+  class_getter unique_app_key : String = ""
+  class_getter database_name : String? = nil
+  class_getter mongo_uri : String = ""
 end
