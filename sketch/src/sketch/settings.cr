7ddafe44ab02ff05edc9473e488d26246cb29854
kebasyaty <kebasyaty@gmail.com>
2024-05-15 15:59:49 +0300

    update settings

--- sketch/src/sketch/settings.cr
+++ sketch/src/sketch/settings.cr
@@ -1,7 +1,11 @@
 # Settings for your web application.
 module Sketch::Settings
-  class_getter app_name : String = ""
-  class_getter unique_app_key : String = ""
+  # Maximum 44 characters.
+  class_getter app_name : String = "AppName"
+  # Match regular expression: /^[a-zA-Z0-9]{16}$/
+  # To generate a key (This is not an advertisement): https://randompasswordgen.com/
+  class_getter unique_app_key : String = "5yh83a5yn7py97j8"
+  # Maximum 60 characters.
   class_getter! database_name : String
   # https://elbywan.github.io/cryomongo/Mongo/Client.html
   class_getter mongo_uri : String = "mongodb://localhost:27017"
