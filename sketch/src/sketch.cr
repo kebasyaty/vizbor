74fac6c78192fe282362e0c1100e5a58b2e56873
kebasyaty <kebasyaty@gmail.com>
2024-05-15 15:45:51 +0300

    update

--- sketch/src/sketch.cr
+++ sketch/src/sketch.cr
@@ -4,4 +4,12 @@ require "./sketch/settings"
 
 module Sketch
   VERSION = "0.1.0"
+
+  # Initialize locale.
+  # https://github.com/crystal-i18n/i18n
+  I18n.config.loaders << I18n::Loader::YAML.new("config/locales")
+  I18n.config.default_locale = :en
+  I18n.init
+
+  puts Sketch::Settings.mongo_uri
 end
--- sketch/src/sketch/settings.cr
+++ sketch/src/sketch/settings.cr
@@ -2,6 +2,7 @@
 module Sketch::Settings
   class_getter app_name : String = ""
   class_getter unique_app_key : String = ""
-  class_getter database_name : String? = nil
-  class_getter mongo_uri : String = ""
+  class_getter! database_name : String
+  # https://elbywan.github.io/cryomongo/Mongo/Client.html
+  class_getter mongo_uri : String = "mongodb://localhost:27017"
 end
