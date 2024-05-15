57bc0549d87c48fdd9579cbaaf5462896d8e74ba
kebasyaty <kebasyaty@gmail.com>
2024-05-15 17:05:02 +0300

    update

--- sketch/src/vizbor/settings.cr
+++ sketch/src/vizbor/settings.cr
@@ -11,7 +11,11 @@ module Vizbor::Settings
   # https://elbywan.github.io/cryomongo/Mongo/Client.html
   class_getter mongo_uri : String = "mongodb://localhost:27017"
   class_getter default_locale : Symbol = :en
-  class_getter local_domain : String = ""
-  class_getter site_domain : String = ""
+  class_getter local_domain : String = "127.0.0.1"
+  class_getter site_domain : String = "www.site-name.net"
   class_getter port : UInt32 = 3000
+  # Maximum upload content size for a web form.
+  # 1 MB = 1048576 Bytes (in binary).
+  # Default: 1048576 * 2 = 2097152 = 2 MB
+  class_getter max_upload_size : UInt32 = 2097152
 end
