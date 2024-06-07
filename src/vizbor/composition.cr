8c1cf9f83adf61f59a7d316a7eca0dac7bc745de
kebasyaty <kebasyaty@gmail.com>
2024-06-07 20:39:16 +0300

    Update alias ServiceComposition

--- src/vizbor/composition.cr
+++ src/vizbor/composition.cr
@@ -2,7 +2,10 @@
 module Vizbor::Composition
   extend self
 
-  alias ServiceComposition = {service: {title: String, icon: String}, collections: Array({title: String, model_key: String, fields: Array({field: String, title: String})})}
+  alias ServiceComposition = NamedTuple(
+    service: {title: String, icon: String},
+    collections: Array({title: String, model_key: String, fields: Array({field: String, title: String})}),
+  )
 
   class_getter menu_structure : Array(ServiceComposition) = Array(ServiceComposition).new
 
